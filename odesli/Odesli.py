0b286afc023734382a8a2b415e1083d64390623c
suhail-c <106970070+suhail-c@users.noreply.github.com>
2023-10-08 09:31:44 +0530

    Update Odesli.py

--- odesli/Odesli.py
+++ odesli/Odesli.py
@@ -19,7 +19,7 @@ class Odesli():
             params['key'] = self.key
         async with httpx.AsyncClient() as client:
             requestResult = await client.get(f'{ROOT}/{LINKS_ENDPOINT}', params=params)
-        requestResult.raise_for_status()
+        await requestResult.raise_for_status()
         result = json.loads(requestResult.content.decode())
         resultType = next(iter(result['entitiesByUniqueId'].values()))['type']
         if resultType == 'song':
